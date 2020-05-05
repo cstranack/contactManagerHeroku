0f0e4436a1a3b88389d586853d1ff06c0b5007ca
Claire Stranack <claire@stranack.co.uk>
2020-05-05 11:49:20 +0100

    fixed port error

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var passport = require('passport');
 var session = require('express-session');
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const mongoURL = process.env.mongoURL || 'mongodb://localhost:27017/handlebars';
 
 //requires a specific function 
